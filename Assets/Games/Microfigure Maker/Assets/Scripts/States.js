0af8b95e796a6b64fa84741830c172ee6807e20e
FarawayDrip30 <66689307+FarawayDrip30@users.noreply.github.com>
2021-12-12 12:50:17 +0000

    microfigure maker

---
+++ Assets/Games/Microfigure Maker/Assets/Scripts/States.js
@@ -0,0 +1,39 @@
+function changeState(state){
+    currentState.end();
+    currentState = state;
+}
+
+class State{
+    constructor(){
+
+    }
+
+    main(){
+
+    }
+    render(){
+
+    }
+    onClick(evt){
+
+    }
+    end(){
+
+    }
+}
+
+class PlayState extends State{
+    constructor(){
+        super();
+        
+        this.body = new Sprite(createImage("Assets/Sprites/microfigures/default.png"),0,0,577,432,0,0,577,432,0,0);
+        this.eyelashes = new Sprite(createImage("Assets/Sprites/eyelashes.png"),0,0,577,432,0,0,577,432,0,0)
+    }
+    render(){
+        clearScreen();
+        this.body.draw();
+        if(eyelashesOn){
+            this.eyelashes.draw();
+        }
+    }
+}
